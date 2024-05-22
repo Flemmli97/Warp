5d77be45651639955f6fd2cb7c30e768da47febf
Flemmli97 <Flemmli97@users.noreply.github.com>
2024-05-22 17:51:23 +0200

    test

--- entry_point.sh
+++ entry_point.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -l
 # Build the wasm pack
+PATH=/usr/local/cargo/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 echo "Starting wasm action"
 cargo install wasm-pack
 cd $GITHUB_WORKSPACE/extensions/warp-ipfs
